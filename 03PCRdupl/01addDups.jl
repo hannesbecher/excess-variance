be2b605639a14993182cf20fbd4cd75b6f075ed4
Hannes Becher <hannesbecher@gmx.de>
2022-12-17 15:51:38 +0000

    more sims

--- 03PCRdupl/01addDups.jl
+++ 03PCRdupl/01addDups.jl
@@ -18,6 +18,7 @@ end
 
 function makeDups(is, lbd; cMax=769200)
   c = 0
+  println("Making dups for IS $is with rate $lbd...")
   dst = Poisson(lbd)
   readerR = FASTQReader(GzipDecompressorStream(open("../02readOverlap/ref1M.fa.IS$is.RW.fq.gz")))  
   
@@ -41,16 +42,28 @@ function makeDups(is, lbd; cMax=769200)
   close(writerL)
 end # function
  
-makeDups(300, 0.10)
-makeDups(300, 0.25)
-makeDups(300, 0.40)
-makeDups(300, 0.55)
-makeDups(300, 0.70)
+#makeDups(300, 0.10)
+#makeDups(300, 0.25)
+#makeDups(300, 0.40)
+#makeDups(300, 0.55)
+#makeDups(300, 0.70)
+#makeDups(300, 1)
+#makeDups(300, 1.5)
+#makeDups(300, 2.5)
+#makeDups(300, 5)
+makeDups(300, 10)
+makeDups(300, 20)
+makeDups(300, 50)
 
-makeDups(500, 0.10)
-makeDups(500, 0.25)
-makeDups(500, 0.40)
-makeDups(500, 0.55)
-makeDups(500, 0.70)
-
-
+#makeDups(500, 0.10)
+#makeDups(500, 0.25)
+#makeDups(500, 0.40)
+#makeDups(500, 0.55)
+#makeDups(500, 0.70)
+#makeDups(500, 1)
+#makeDups(500, 1.5)
+#makeDups(500, 2.5)
+#makeDups(500, 5)
+makeDups(500, 10)
+makeDups(500, 20)
+makeDups(500, 50)
--- 03PCRdupl/03analyseSim.R
+++ 03PCRdupl/03analyseSim.R
@@ -10,13 +10,18 @@ plot(sps[[2]], xlim=c(150, 250))
 
 plot(sps[[3]], xlim=c(150, 250))
 plot(sps[[4]], xlim=c(150, 250))
+plot(sps[[11]], xlim=c(150, 250))
+plot(sps[[12]], xlim=c(150, 250))
+plot(sps[[23]], xlim=c(150, 250))
+plot(sps[[24]], xlim=c(150, 250))
+plot(sps[[25]], xlim=c(150, 250))
 
 makeLong <- function(dat){
   rep(dat[,1], dat[,2])
 }
 
 
-pars <- t(sapply(1:12, function(x){
+pars <- t(sapply(1:length(fs), function(x){
   dat <- makeLong(sps[[x]]@data)
   glm00 <- MASS::glm.nb(dat~1)
   c(mean=exp(coef(summary(glm00))[1]),
@@ -25,23 +30,33 @@ pars <- t(sapply(1:12, function(x){
 
 varsVR <- apply(pars, 1, function(x) x[1] + x[1]^2/x[2])
 df <- data.frame(pars, var=varsVR, run=fs)
-
-df$relVar <- varsVR/varsVR[rep(c(6, 12), each=6)]
-
-df <- df[c(6, 1, 2, 3, 4, 5, 12, 7, 8,9, 10, 11),]
-df$rep <- rep(c(0, 0.1, 0.25, 0.4, 0.55, 0.7),2)
+df$relVar <- varsVR/varsVR[rep(c(13, 26), each=13)]
+df <- df[c(13, 1, 2, 3, 4, 5, 7, 6, 9, 11, 8, 10, 12, c(13, 1, 2, 3, 4, 5, 7, 6, 9, 11, 8, 10, 12)+13),]
+df$rep <- rep(c(0, 0.1, 0.25, 0.4, 0.55, 0.7, 1, 1.5, 2.5, 5, 10, 20, 50),2)
 
 #png("repVar.png", width=7, height=5.5, res=150, units="in")
 plot(relVar ~ rep,
      data=df,
      xlab="Avg. duplicates per read",
-     ylab="Rel. var compared to 0 repetition")
+     ylab="Rel. var compared to 0 repetition",
+     col=rep(c(1, 2), each=13),
+     log="")
 grid()
 lm01 <- lm(relVar ~ rep,
            data=df)
+lm02 <- lm(relVar ~ rep + I(rep^2),
+           data=df)
 summary(lm01)
+summary(lm02)
+
 abline(lm01)
+xVals <- seq(0, 50, 0.05)
+predY <- predict(lm02, data.frame(rep=xVals))
+lines(xVals, predY)
+
 #dev.off()
 par(mfrow=c(2,2))
 plot(lm01)
+plot(lm02)
+
 par(mfrow=c(1,1))
